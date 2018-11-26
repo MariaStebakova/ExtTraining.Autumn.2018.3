21ef3067c28919b15af69f1c99b2f02976be3ce1
Maria Stebakova <stebakova1998@gmail.com>
2018-11-26 11:05:44 +0300

    No1

---
+++ No1.Solution.Tests/PasswordCheckerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace No1.Solution.Tests
+{
+    [TestFixture]
+    public class PasswordCheckerTests
+    {
+        [TestCase(null, false, "null is null arg")]
+        [TestCase("", false, " is empty ")]
+        [TestCase("1111", false, "1111 length too short")]
+        [TestCase("1111111111111111", false, "1111111111111111 length too long")]
+        [TestCase("1111111111", false, "11111111111 hasn't alphanumerical chars")]
+        [TestCase("aaaaaaaaaa", false, "aaaaaaaaaa hasn't digits")]
+        public (bool, string) Test_WithGivenConditions(string password, bool result, string stringResult)
+        {
+            var passwordChecker = new PasswordCheckerService(new SqlRepository(), null);
+            return passwordChecker.VerifyPassword(password);
+        }
+
+        [TestCase("aaa,12345", false, "aaa,12345 doesn't correspond some of custom conditions")]
+        public (bool, string) Test_WithCustomCondition(string password, bool result, string stringResult)
+        {
+            Func<string, bool> func1 = s => s.Any(char.IsPunctuation);
+            Func<string, bool> func2 = s => !s.Any(char.IsUpper);
+            var passwordChecker = new PasswordCheckerService(new SqlRepository(), new Func<string, bool>[] {func1, func2});
+            return passwordChecker.VerifyPassword(password);
+        }
+    }
+}
---
+++ No1.Solution/PasswordCheckerService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No1.Solution
+{
+    public class PasswordCheckerService
+    {
+        private readonly IRepository repository;
+        private readonly Func<string, bool>[] validationConditions;
+
+        public PasswordCheckerService(IRepository repository, params Func<string, bool>[] validationCases)
+        {
+            this.repository = repository;
+            this.validationConditions = validationCases;
+        }
+
+        public (bool, string) VerifyPassword(string password)
+        {
+            if (password == null)
+                throw new ArgumentException($"{password} is null arg");
+
+            if (password == string.Empty)
+                return (false, $"{password} is empty ");
+
+            // check if length more than 7 chars 
+            if (password.Length <= 7)
+                return (false, $"{password} length too short");
+
+            // check if length more than 10 chars for admins
+            if (password.Length >= 15)
+                return (false, $"{password} length too long");
+
+            // check if password contains at least one alphabetical character 
+            if (!password.Any(char.IsLetter))
+                return (false, $"{password} hasn't alphanumerical chars");
+
+            // check if password contains at least one digit character 
+            if (!password.Any(char.IsNumber))
+                return (false, $"{password} hasn't digits");
+
+            if (validationConditions == null)
+            {
+                repository.Create(password);
+
+                return (true, "Password is Ok. User was created");
+            }
+
+            //check if password correspond custom conditions
+            foreach (var func in validationConditions)
+            {
+                if (!func(password))
+                    return (false, $"{password} doesn't correspond some of custom conditions");
+            }
+
+            repository.Create(password);
+
+            return (true, "Password is Ok. User was created");
+        }
+    }
+
+    //Интерфейс необходим для устранения привязки к определенному репозиторию
+    //Массив делегатов выбран, потому что в условиии сказано что может быть несколько дополнительных условий
+    //проверки пароля
+}
